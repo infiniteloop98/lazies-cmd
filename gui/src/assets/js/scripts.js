de227851a2914455d6cb61acedf1ee574b78efd0
sajjadN7798 <sajjad.n18@outlook.com>
2021-12-15 01:21:15 +0330

    1. Create action complete 2. Update action complete

--- gui/src/assets/js/scripts.js
+++ gui/src/assets/js/scripts.js
@@ -4,6 +4,8 @@ const { PythonShell } = require("python-shell"),
     enginePath: path.join(__dirname + `../../../engine`),
     commandList: document.querySelector("#command-list"),
     addModal: "add-command-modal",
+    pythonRootScript: "main.py",
+    toastContainer: document.querySelector(".toast-container"),
   };
 
 PythonShell.defaultOptions = {
@@ -12,55 +14,159 @@ PythonShell.defaultOptions = {
 };
 
 window.onload = () => {
-  Alpine.store("labels", {
-    commands: "Commands",
-    alias: "Alias",
-    command: "Command",
-    add: "Add",
-    addCommand: "Add command",
-    close: "Close",
-    save: "Save",
-    actions: "Actions",
-    update: "Update",
-    remove: "Remove",
-  });
-  Alpine.store("modals", {
-    add: "add-command-modal",
-  });
+  handleSwitch();
 };
 
+//CRUD functions
+
+/**
+ * Get list of commands
+ * @returns {Array}
+ */
 async function getCommands() {
-  const { commandList } = state;
-  const commands = await executePythonScript("main.py", {
-    args: ["GET"],
-  });
+  const { commandList } = state,
+    commands = await executePythonScript(state.pythonRootScript, {
+      args: ["GET"],
+    });
   commandList.querySelector("#spinner").classList.add("d-none");
-  return commands;
+  return commands.reverse();
 }
 
-function getEngineFilesPath(filename) {
-  return `${enginePath}/${filename}`;
-}
-
-async function updateCommand(command) {
-  const update = await executePythonScript("main.py", {
-    args: ["UPDATE"],
-  });
-}
-
+/**
+ * Add new command
+ * @param {Node} e
+ */
 async function addCommand(e) {
-  const { alias, command } = buildFormData(e);
-  await executePythonScript("main.py", {
-    args: ["ADD", alias, command],
-  });
-  hideModal(state.addModal);
+  const {
+      alias,
+      command,
+      withPrefix = false,
+      withSuffix = false,
+    } = buildFormData(e),
+    aliasInput = document.getElementById("alias"),
+    commandInput = document.getElementById("command"),
+    withPrefixCheckbox = document.getElementById("with-prefix"),
+    withSuffixCheckbox = document.getElementById("with-suffix");
+  if (alias === "") {
+    aliasInput.focus();
+    showToast({ title: "Error", message: "Alias is required." }, "error");
+    return false;
+  }
+  if (command === "") {
+    commandInput.focus();
+    showToast({ title: "Error", message: "Command is required." }, "error");
+    return false;
+  }
+  await executePythonScript(
+    state.pythonRootScript,
+    {
+      args: ["CREATE", alias, command, withPrefix, withSuffix],
+    },
+    onAdd
+  );
+
+  function onAdd() {
+    aliasInput.value = null;
+    commandInput.value = null;
+    withPrefixCheckbox.checked = false;
+    withSuffixCheckbox.checked = false;
+    dispatch("command-added");
+  }
+  return true;
 }
 
+/**
+ * Update command
+ * @param {String} command
+ */
+async function updateCommand(e) {
+  const {
+      alias,
+      command,
+      withPrefix = false,
+      withSuffix = false,
+    } = buildFormData(e),
+    aliasInput = document.getElementById("edit-alias"),
+    commandInput = document.getElementById("edit-command"),
+    withPrefixCheckbox = document.getElementById("edit-with-prefix"),
+    withSuffixCheckbox = document.getElementById("edit-with-suffix");
+  if (alias === "") {
+    aliasInput.focus();
+    showToast({ title: "Error", message: "Alias is required." }, "error");
+    return false;
+  }
+  if (command === "") {
+    commandInput.focus();
+    showToast({ title: "Error", message: "Command is required." }, "error");
+    return false;
+  }
+
+  const response = await executePythonScript(
+    state.pythonRootScript,
+    {
+      args: ["UPDATE", alias, command, withPrefix, withSuffix],
+    },
+    onUpdate
+  );
+
+  function onUpdate() {
+    aliasInput.value = null;
+    commandInput.value = null;
+    withPrefixCheckbox.checked = false;
+    withSuffixCheckbox.checked = false;
+    dispatch("command-updated");
+  }
+  return true;
+}
+
+// End of CURD functions
+
+// Helper functions
+
+function handleSwitch() {
+  const switchs = document.querySelectorAll(
+    'input[type="checkbox"].form-check-input'
+  );
+  switchs.forEach((item) => {
+    item.addEventListener("change", ({ target }) => {
+      const { checked } = target;
+      if (checked) target.value = true;
+      else target.value = false;
+    });
+  });
+}
+
+/**
+ *
+ * @param {String} event
+ * @param {Node} element
+ * @param {Object} options
+ * @returns
+ */
+function dispatch(event, element = document.body, options = {}) {
+  return element.dispatchEvent(new CustomEvent(event, options));
+}
+
+/**
+ * Get form node and return form data object
+ * @param {Node} form
+ * @returns
+ */
 function buildFormData(form) {
   return Object.fromEntries(new FormData(form).entries());
 }
 
-async function executePythonScript(fileName, extendedOptions = {}) {
+/**
+ * Python script executer
+ * @param {String} fileName
+ * @param {Object} extendedOptions
+ * @returns
+ */
+async function executePythonScript(
+  fileName,
+  extendedOptions = {},
+  callback = () => {}
+) {
   let options = {
     mode: "json",
     ...extendedOptions,
@@ -70,14 +176,156 @@ async function executePythonScript(fileName, extendedOptions = {}) {
     execute.on("message", (result) => {
       return resolve(result);
     });
+    callback();
   });
   return result;
 }
 
-function showModal(modal) {
-  return new bootstrap.Modal(modal).show();
+/**
+ * Show bootstrap modal
+ * @param {Node} modal
+ * @returns
+ */
+function showModal(modalId) {
+  return new bootstrap.Modal(document.getElementById(modalId)).show();
 }
 
+/**
+ * Hide bootstrap modal
+ * @param {String} modalId
+ */
 function hideModal(modalId) {
   document.querySelector(`#${modalId} .modal-header button.btn-close`).click();
+}
+
+function showToast(
+  content,
+  type = "success",
+  delay = 5000,
+  placement = "bottom-right"
+) {
+  const { toastContainer } = state;
+  removeClass(
+    toastContainer,
+    "top-0 end-0 top-0 end-0 bottom-0 end-0 bottom-0 start-0"
+  );
+  let toastClassList = "toast text-white",
+    toastContainerClassList = "p-5",
+    options = {
+      delay,
+    };
+  switch (type) {
+    case "error":
+      toastClassList += " bg-danger";
+      break;
+    case "info":
+      toastClassList += " bg-info";
+      break;
+    case "warning":
+      toastClassList += " bg-warning";
+      break;
+    default:
+      toastClassList += " bg-success";
+      break;
+  }
+  switch (placement) {
+    case "top-right":
+      toastContainerClassList += " top-0 end-0";
+      break;
+    case "top-left":
+      toastContainerClassList += " top-0 end-0";
+      break;
+    case "bottom-left":
+      toastContainerClassList += " bottom-0 end-0";
+      break;
+    default:
+      toastContainerClassList += " bottom-0 start-0";
+      break;
+  }
+  addClass(toastContainer, toastContainerClassList);
+  if (Array.isArray(content)) {
+    content.map((item) => {
+      const toast = document.createElement("div");
+      initialToast(toast, toastClassList);
+      const { title, message } = item;
+      toast.innerHTML = buildToastTemplate(title, message);
+      toastContainer.appendChild(toast);
+      return new bootstrap.Toast(toast, options).show();
+    });
+    return;
+  }
+  const toast = document.createElement("div");
+  initialToast(toast, toastClassList);
+  const { title, message } = content;
+  toast.innerHTML = buildToastTemplate(title, message);
+  toastContainer.appendChild(toast);
+  onHideToast();
+  return new bootstrap.Toast(toast, options).show();
+}
+
+function onHideToast() {
+  const toasts = document.querySelectorAll(".toast");
+  toasts.forEach((toast) => {
+    toast.addEventListener("hidden.bs.toast", function () {
+      toast.remove();
+    });
+  });
+}
+
+const initialToast = (toast, toastClassList = "toast") => {
+  toast.className += toastClassList;
+  setMultipleAttribute(toast, {
+    role: "alert",
+    "aria-live": "assertive",
+    "aria-atomic": true,
+  });
+};
+
+const buildToastTemplate = (title, body) => {
+  return `<div class="toast-header">
+    <strong class="me-auto toast-title">${title}</strong>
+    <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>'
+    </div>
+    <div class="toast-body">${body}</div>`;
+};
+
+function setMultipleAttribute(element, attributes) {
+  for (const key in attributes) {
+    if (attributes.hasOwnProperty(key))
+      element.setAttribute(key, attributes[key]);
+  }
+}
+
+function addClass(element, classes) {
+  if (containMultipleClass(classes)) {
+    const classListArray = classes.split(" ");
+    classListArray.forEach((item) => {
+      !hasClass(element, item) && element.classList.add(item);
+    });
+    return true;
+  }
+  if (!hasClass(element, classes)) {
+    element.classList.add(classes);
+    return;
+  }
+}
+
+function removeClass(element, classes) {
+  if (containMultipleClass(classes)) {
+    const classListArray = classes.split(" ");
+    classListArray.forEach((item) => {
+      hasClass(element, item) && element.classList.remove(item);
+    });
+    return true;
+  }
+  hasClass(element, classes) && element.classList.remove(classes);
+  return true;
+}
+
+function containMultipleClass(string) {
+  return string.indexOf(" ");
+}
+
+function hasClass(element, className) {
+  return element.classList.contains(className);
 }
--- gui/src/index.js
+++ gui/src/index.js
@@ -1,6 +1,5 @@
 const { app, BrowserWindow } = require("electron"),
-  path = require("path"),
-  { PythonShell } = require("python-shell");
+  path = require("path");
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
