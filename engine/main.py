92104f01a7c9361e5c0bafaa1402a0cdf10c5abe
sajjadN7798 <sajjad.n18@outlook.com>
2021-12-15 01:20:22 +0330

    1. Create method complete 2. Update method writing but has some bug

--- engine/main.py
+++ engine/main.py
@@ -1,25 +1,65 @@
 import json
 import sys
-
 from modules.doskey.doskey import Doskey
+
+
+def showException(title: str, message: str = ""):
+    response = {"title": title, "message": f"{message if message else ''}"}
+    print(json.dumps(response))
 
 
 class App:
     def __init__(self):
-        arguments = sys.argv
-        action = arguments[1]
-        doskey = Doskey()
+        self.arguments = sys.argv
+        action = None
+        try:
+            action = self.arguments[1]
+        except IndexError:
+            showException("Action error", "Action not found.")
+        except:
+            showException("Unexpected error", "Unknown error!")
+        self.doskey = Doskey()
+        if action in ["CREATE", "UPDATE"] and len(self.arguments) > 1:
+            alias, command, options = self.getArguments()
+            self.alias = alias
+            self.command = command
+            self.options = options
+
         if action == "GET":
-            commands = json.dumps(doskey.get())
-            print(commands)
+            self.get()
+            return
+        elif action == "CREATE":
+            self.create()
             return
         elif action == "UPDATE":
+            self.update()
             return
-        elif action == "ADD":
-            alias = arguments[2]
-            command = arguments[3]
-            doskey.add(alias, command)
-            print(json.dump(True))
+
+    def get(self):
+        commands = json.dumps(self.doskey.get())
+        print(commands)
+
+    def create(self):
+        if self.alias and self.command and self.options:
+            self.doskey.create(self.alias, self.command, self.options)
+            print(json.dumps(True))
+        print(json.dumps(False))
+
+    def update(self):
+        if self.alias and self.command and self.options:
+            self.doskey.update(self.alias, self.command, self.options)
+            print(json.dumps(True))
+        print(json.dumps(False))
+
+    def getArguments(self):
+        alias = self.arguments[2]
+        command = self.arguments[3]
+        with_prefix = True if self.arguments[4] == 'true' else False
+        with_suffix = True if self.arguments[5] == 'true' else False
+        return alias, command, {
+            "with_prefix": with_prefix,
+            "with_suffix": with_suffix
+        }
 
 
 App()
